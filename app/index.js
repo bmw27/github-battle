8887390c8ef5561818774fad5713296f232f07e9
Brandon Williams <bmwill@outlook.com>
2019-12-17 20:34:30 -0700

    finished (Project) Repositories Grid

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import Popular from "./components/Popular";
+import "./index.css";
 
 class App extends React.Component {
   render() {
